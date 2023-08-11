fcace7335b44f96e15f8c391a325e49cb5524df2
Sergey <ssnovak@stud.kpfu.ru>
2023-08-11 22:45:30 +0300

    + hard code: create_post and get_posts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -11,6 +12,12 @@ class Post(BaseModel):
     rating: int | None = None
 
 
+my_posts = [
+    {'title': 'title1', 'content': 'content1', 'id': 1},
+    {'title': 'title2', 'content': 'content2', 'id': 2}
+]
+
+
 @app.get('/')
 async def root():
     return {'message': 'Hello world'}
@@ -18,10 +25,12 @@ async def root():
 
 @app.get('/posts')
 def get_posts():
-    return {'data': 'There are your posts'}
+    return {'data': my_posts}
 
 
 @app.post('/create-post')
 def create_post(post: Post):
-    print(post.model_dump())
-    return{'data': post}
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 100000)
+    my_posts.append(post_dict)
+    return{'data': post_dict}
