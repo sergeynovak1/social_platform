dcd1c4200d8d4782b04bacc332597bd2cd08c249
Sergey <ssnovak@stud.kpfu.ru>
2023-08-23 19:41:42 +0300

    + simple caches

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
+import time
+
+from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from redis import asyncio as aioredis
 
 from src.post import router as post
 from src.vote import router as vote
@@ -11,3 +17,16 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.on_event("startup")
+async def startup_event():
+    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+
+@app.get("/long_operation")
+@cache(expire=30)
+def get_long_op():
+    time.sleep(2)
+    return {'message': "Много много данных, которые вычислялись сто лет"}
